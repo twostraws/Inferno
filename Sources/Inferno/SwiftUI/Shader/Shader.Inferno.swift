65fedb38aefb871cb729b20c84070bec84775840
Harshil Shah <harshilshah1910@outlook.com>
2023-12-11 11:22:50 +0530

    Added a Shader.inferno namespace

---
+++ Sources/Inferno/SwiftUI/Shader/Shader.Inferno.swift
@@ -0,0 +1,15 @@
+//
+// Shader.Inferno.swift
+// Inferno
+// https://www.github.com/twostraws/Inferno
+// See LICENSE for license information.
+//
+
+import SwiftUI
+
+@available(iOS 17, macOS 14, macCatalyst 17, tvOS 17, visionOS 1, *)
+public extension Shader {
+	
+	/// A namespace for all shaders exported by Inferno
+	enum inferno {}
+}
