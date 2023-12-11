70da637093c0aab2b63f6811f2d13017b58e4a3d
Harshil Shah <harshilshah1910@outlook.com>
2023-12-11 11:23:34 +0530

    Added a Shader.inferno.color namespace

---
+++ Sources/Inferno/SwiftUI/Shader/Shader.Inferno.Color.swift
@@ -0,0 +1,16 @@
+//
+// Shader.Inferno.swift
+// Inferno
+// https://www.github.com/twostraws/Inferno
+// See LICENSE for license information.
+//
+
+import SwiftUI
+
+@available(iOS 17, macOS 14, macCatalyst 17, tvOS 17, visionOS 1, *)
+public extension Shader.inferno {
+	
+	/// A namespace for all `colorEffect` shaders exported by Inferno
+	enum color {}
+}
+
