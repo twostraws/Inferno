d7d649bea692385323478401bdaf474e0bfd237e
Harshil Shah <harshilshah1910@outlook.com>
2023-12-11 11:23:55 +0530

    Added a convenience function to generate the animated gradient fill shader

--- Sources/Inferno/SwiftUI/Shader/Shader.Inferno.Color.swift
+++ Sources/Inferno/SwiftUI/Shader/Shader.Inferno.Color.swift
@@ -14,3 +14,21 @@ public extension Shader.inferno {
 	enum color {}
 }
 
+@available(iOS 17, macOS 14, macCatalyst 17, tvOS 17, visionOS 1, *)
+public extension Shader.inferno.color {
+	/// A shader that generates a constantly cycling color gradient, centered
+	/// on the input view.
+	///
+	/// - Parameters:
+	///   - size: The size of the whole image, in user-space.
+	///   - elapsedTime: The number of elapsed seconds since the shader was created
+	static func animatedGradientFill(
+		size: CGSize,
+		elapsedTime: Double
+	) -> Shader {
+		InfernoShaderLibrary.animatedGradientFill(
+			.float2(size),
+			.float(elapsedTime)
+		)
+	}
+}
