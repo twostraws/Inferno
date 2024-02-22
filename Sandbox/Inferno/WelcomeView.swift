4d9d650bf7768b50dc3504e13463fd95c58d68e0
Nick Kohrn <bnkohrn@icloud.com>
2024-02-22 13:16:48 -0500

    Allow welcome view to scroll
    Allow the welcome view to scroll in order to better handle Dynamic Type and orientation changes

--- Sandbox/Inferno/WelcomeView.swift
+++ Sandbox/Inferno/WelcomeView.swift
@@ -13,33 +13,42 @@ struct WelcomeView: View {
     @State private var logoHover = false
 
     var body: some View {
-        VStack(spacing: 10) {
-            Spacer()
+        GeometryReader { geometry in
+            ScrollView(showsIndicators: false) {
+                VStack(spacing: 10) {
+                    Spacer()
 
-            Image(.logo)
-                .padding(.bottom, 10)
+                    Image(.logo)
+                        .padding(.bottom, 10)
 
-            Link("github.com/twostraws/inferno", destination: URL(string: "https://github.com/twostraws/inferno")!)
-                .font(.title2.bold())
+                    Link("github.com/twostraws/inferno", destination: URL(string: "https://github.com/twostraws/inferno")!)
+                        .font(.title2.bold())
 
-            Text("This is a small sandbox so you can see the various shaders in action. To use the shaders in your own code, follow the instructions in the documentation rather than taking code from here.")
-                .multilineTextAlignment(.center)
-                .font(.title3)
+                    Text("This is a small sandbox so you can see the various shaders in action. To use the shaders in your own code, follow the instructions in the documentation rather than taking code from here.")
+                        .multilineTextAlignment(.center)
+                        .font(.title3)
 
-            Spacer()
-            Spacer()
+                    Spacer()
+                    Spacer()
 
-            Link(destination: URL(string: "https://www.hackingwithswift.com")!) {
-                VStack {
-                    Image(.HWS)
-                        .renderingMode(logoHover ? .template : .original)
-                    Text("A Hacking with Swift Project")
+                    Link(destination: URL(string: "https://www.hackingwithswift.com")!) {
+                        VStack {
+                            Image(.HWS)
+                                .renderingMode(logoHover ? .template : .original)
+                            Text("A Hacking with Swift Project")
+                        }
+                    }
+                    .onHover { logoHover = $0 }
+                    .foregroundStyle(.white)
+
+                    Spacer()
                 }
+                .padding()
+                .frame(width: geometry.size.width)
+                .frame(minHeight: geometry.size.height)
             }
-            .onHover { logoHover = $0 }
-            .foregroundStyle(.white)
+            .scrollBounceBehavior(.basedOnSize)
         }
         .navigationSubtitle("Welcome to the Shader Sandbox")
-        .padding()
     }
 }
