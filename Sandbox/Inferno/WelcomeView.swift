bb98e529d349a238133f373d891792ccc9c02385
Nick Kohrn <bnkohrn@icloud.com>
2024-03-18 07:34:09 -0400

    Replace GeometryReader with ViewThatFits

--- Sandbox/Inferno/WelcomeView.swift
+++ Sandbox/Inferno/WelcomeView.swift
@@ -13,42 +13,46 @@ struct WelcomeView: View {
     @State private var logoHover = false
 
     var body: some View {
-        GeometryReader { geometry in
-            ScrollView(showsIndicators: false) {
-                VStack(spacing: 10) {
-                    Spacer()
+        ViewThatFits(in: .vertical) {
+            content
 
-                    Image(.logo)
-                        .padding(.bottom, 10)
-
-                    Link("github.com/twostraws/inferno", destination: URL(string: "https://github.com/twostraws/inferno")!)
-                        .font(.title2.bold())
-
-                    Text("This is a small sandbox so you can see the various shaders in action. To use the shaders in your own code, follow the instructions in the documentation rather than taking code from here.")
-                        .multilineTextAlignment(.center)
-                        .font(.title3)
-
-                    Spacer()
-                    Spacer()
-
-                    Link(destination: URL(string: "https://www.hackingwithswift.com")!) {
-                        VStack {
-                            Image(.HWS)
-                                .renderingMode(logoHover ? .template : .original)
-                            Text("A Hacking with Swift Project")
-                        }
-                    }
-                    .onHover { logoHover = $0 }
-                    .foregroundStyle(.white)
-
-                    Spacer()
-                }
-                .padding()
-                .frame(width: geometry.size.width)
-                .frame(minHeight: geometry.size.height)
+            /*
+             If the content's ideal size does not fit vertically, then allow the content to scroll.
+             This is useful when adapting to Dynamic Type changes.
+             */
+            ScrollView {
+                content
             }
-            .scrollBounceBehavior(.basedOnSize)
         }
         .navigationSubtitle("Welcome to the Shader Sandbox")
+    }
+
+    @ViewBuilder
+    private var content: some View {
+        VStack(spacing: 10) {
+            Image(.logo)
+                .padding(.bottom, 10)
+
+            Link("github.com/twostraws/inferno", destination: URL(string: "https://github.com/twostraws/inferno")!)
+                .font(.title2.bold())
+
+            Text("This is a small sandbox so you can see the various shaders in action. To use the shaders in your own code, follow the instructions in the documentation rather than taking code from here.")
+                .multilineTextAlignment(.center)
+                .font(.title3)
+
+            Spacer()
+            Spacer()
+
+            Link(destination: URL(string: "https://www.hackingwithswift.com")!) {
+                VStack {
+                    Image(.HWS)
+                        .renderingMode(logoHover ? .template : .original)
+                    Text("A Hacking with Swift Project")
+                }
+            }
+            .onHover { logoHover = $0 }
+            .foregroundStyle(.white)
+        }
+        .padding()
     }
 }
